168501bdcdf529a5df4024aebc08951b83eee9cc
RianOntu <rianontu13@gmail.com>
2025-11-17 14:08:38 +0600

    solved 3rd problem

--- solution.ts
+++ solution.ts
@@ -17,5 +17,18 @@ const getLength = (value: string | number[] | string[]): number | undefined => {
   return value.length;
 };
 
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 
+  getDetails() {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
 
+const person1 = new Person('John Doe', 30);
+person1.getDetails()
